1e908b66e174decc2789930ba106e9a3f97b0837
a.breezy <ambrose.wilkins@gmail.com>
2021-12-19 17:30:10 -0500

    changed port -> PORT ln:8

--- server.js
+++ server.js
@@ -5,8 +5,7 @@ const path = require("path");
 const apiRoutes = require("./routes/apiRoutes");
 const html = require("./routes/htmlRoutes");
 
-
-const PORT = process.env.port || 3001;
+const PORT = process.env.PORT || 3001;
 
 // middleware
 app.use(express.urlencoded({ extended: true }));
@@ -17,5 +16,5 @@ app.use("/api", apiRoutes);
 app.use("/", html);
 
 app.listen(PORT, () => {
-	console.log(`API server now on pcort ${PORT}!`);
+	console.log(`API server now on port ${PORT}!`);
 });
